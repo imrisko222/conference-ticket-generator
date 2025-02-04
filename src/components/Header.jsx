bc43adf606e3ccafe1749b811c388ab92739e776
Roman Klaška <roman.klaska@Mac-mini-uzivatela-Roman.local>
2025-02-04 20:15:53 +0100

    spraveny len header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderIcon from "../assets/images/logo-mark.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={HeaderIcon} alt="HeaderIcon" />
+      <h2>Coding Conf</h2>
+    </header>
+  );
+};
+
+export default Header;
