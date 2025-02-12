67723eefe47b52120f37217e8b4a6f0b793e87d4
Roman Klaška <romanklaska@MacBook-Pro-uzivatela-Roman.local>
2025-02-12 15:55:59 +0100

    mierne postylovane, pridany input pre fileUpload a mierne opravena heroSekcia

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,18 +4,22 @@ import styles from './Form.module.css'
 const Form = () => {
   return (
     <form className={styles.formContainer}>
-        <label htmlFor="">
+        <label className={styles.labelStyle} htmlFor="uploadInput">
+Upload Avatar</label>
+        <input type='file' id='uploadInput' className={styles.fileStyle} accept='.jpg, .png' placeholder='Drag and drop or click to upload' />
+
+        <label htmlFor="fullName" className={styles.labelStyle}>
 Full Name</label>
-        <input type='text'/>
+        <input type='text' id='fullName' className={styles.inputStyle}/>
         
-        <label htmlFor="">
+        <label htmlFor="emailAddress" className={styles.labelStyle}>
 Email Address</label>
-        <input type='email' placeholder='example@email.com'/>
+        <input type='email' id='emailAddress' className={styles.inputStyle} placeholder='example@email.com'/>
         
-        <label htmlFor="">
-Full Name</label>
-        <input type='email' placeholder='@yournick'/>
-        <button type='submit'>Generate My Ticket</button>
+        <label htmlFor="userName" className={styles.labelStyle}>
+Github Username</label>
+        <input type='email' id='userName' className={styles.inputStyle} placeholder='@yournick'/>
+        <button className={styles.buttonSubmit} type='submit'>Generate My Ticket</button>
 
     </form>
   )
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from './HeroSection.module.css'
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className={styles.heroStyle}>
         <h1>Your Journey to Coding Cong 2025 Starts Here!</h1>
         <p>Secure your spot at next year's biggest codding conference.</p>
     </div>
