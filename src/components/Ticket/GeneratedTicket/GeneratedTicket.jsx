558c12b731f87dfc6bc4482c48827b827c620abb
Roman Klaška <romanklaska@MacBook-Pro-uzivatela-Roman.local>
2025-02-19 15:35:29 +0100

    par veci pridanych do komponenty generatedTicket

--- src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
+++ src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import style from './GeneratedTicket.module.css'
+import Icon from '../../../assets/images/logo-mark.svg'
 
 const GeneratedTicket = () => {
   return (
 <div class={style.ticketBorder}>
-  <div className={style.partOfTicketOne}></div>
+  <div className={style.partOfTicketOne}>
+    <div className={style.namedSection}>
+
+    <img src={Icon} alt="Conference icon" />
+    <p>Coding Conf</p>
+    </div>
+  </div>
   <div className={style.partOfTicketTwo}></div>
   <div className={style.partOfTicketThree}></div>
 </div>
