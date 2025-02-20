55bc19ad673aaed4db0a22b0f3946fa3329a393e
Roman Klaška <romanklaska@MacBook-Pro-uzivatela-Roman.local>
2025-02-20 15:19:50 +0100

    zaciatok spodnej casti tiketu

--- src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
+++ src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import style from './GeneratedTicket.module.css'
 import Icon from '../../../assets/images/logo-mark.svg'
+import Avatar from '../../../assets/images/image-avatar.jpg'
+
 
 const GeneratedTicket = () => {
   return (
 <div class={style.ticketBorder}>
   <div className={style.partOfTicketOne}>
     <div className={style.namedSection}>
-
-    <img src={Icon} alt="Conference icon" />
-    <p>Coding Conf</p>
+      <img src={Icon} alt="Conference icon" />
+      <p>Coding Conf</p>
+    </div>
+    <div className={style.avatarSection}>
+      <img src={Avatar} alt="Conference attendee’s photo" />
     </div>
   </div>
   <div className={style.partOfTicketTwo}></div>
