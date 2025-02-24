ad1ea57a9f851bf3f979ee350b6bb88e4ddf1e6a
Roman Klaška <roman.klaska@Mac-mini-uzivatela-Roman.local>
2025-02-24 20:26:05 +0100

    ticket spraveny pomocou background.svg

--- src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
+++ src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { useState } from 'react'
-import style from './GeneratedTicket.module.css'
-import Icon from '../../../assets/images/logo-mark.svg'
-import Avatar from '../../../assets/images/image-avatar.jpg'
-
+import React from "react";
+import { useState } from "react";
+import style from "./GeneratedTicket.module.css";
+import Icon from "../../../assets/images/logo-mark.svg";
+import Avatar from "../../../assets/images/image-avatar.jpg";
+import Ticket from "../../../assets/images/ticket_bg.svg";
 
 const GeneratedTicket = () => {
-
   const generatedNumber = () => {
     const randomNumber = Math.floor(10000 + Math.random() * 90000); // generuje číslo od 10000 do 99999
     return `#${randomNumber}`;
   };
-  
+
   const [ticketNumber] = useState(generatedNumber);
 
   return (
-<div class={style.ticketBorder}>
-  <div className={style.partOfTicketOne}>
+    <div class={style.ticketContainer}>
+      {/* <div className={style.partOfTicketOne}>
     <div className={style.namedSection}>
       <img src={Icon} alt="Conference icon" />
       <p>Coding Conf</p>
@@ -32,9 +31,10 @@ const GeneratedTicket = () => {
   <div className={style.partOfTicketTwo}></div>
   <div className={style.partOfTicketThree}>
     <p className={style.generatedNumber}>{ticketNumber}</p>
-  </div>
-</div>
-  )
-}
+  </div> */}
+      {/* <img src={Ticket} alt="ticket" /> */}
+    </div>
+  );
+};
 
-export default GeneratedTicket
+export default GeneratedTicket;
