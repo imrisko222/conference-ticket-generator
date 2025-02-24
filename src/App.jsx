1ccd0f658830e01834b5ac260cdc4c0bf3bbaaee
Roman Klaška <romanklaska@MacBook-Pro-uzivatela-Roman.local>
2025-02-24 16:49:30 +0100

    generovanie cisla v tickete a jeho mierne nastylovanie

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       {/* <Header />
       <HeroSection />
       <Form /> */}
-<TicketComponent />
+      <TicketComponent />
     </section>
   );
 }
--- src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
+++ src/components/Ticket/GeneratedTicket/GeneratedTicket.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 import style from './GeneratedTicket.module.css'
 import Icon from '../../../assets/images/logo-mark.svg'
 import Avatar from '../../../assets/images/image-avatar.jpg'
 
 
 const GeneratedTicket = () => {
+
+  const generatedNumber = () => {
+    const randomNumber = Math.floor(10000 + Math.random() * 90000); // generuje číslo od 10000 do 99999
+    return `#${randomNumber}`;
+  };
+  
+  const [ticketNumber] = useState(generatedNumber);
+
   return (
 <div class={style.ticketBorder}>
   <div className={style.partOfTicketOne}>
@@ -13,7 +22,7 @@ const GeneratedTicket = () => {
       <p>Coding Conf</p>
     </div>
     <div className={style.avatarSection}>
-      <img src={Avatar} alt="Conference attendee’s photo" />
+      <img src={Avatar} alt="Conference attendees photo" />
       <div>
         <p>Meno</p>
         <p>odkaz na github</p>
@@ -21,7 +30,9 @@ const GeneratedTicket = () => {
     </div>
   </div>
   <div className={style.partOfTicketTwo}></div>
-  <div className={style.partOfTicketThree}></div>
+  <div className={style.partOfTicketThree}>
+    <p className={style.generatedNumber}>{ticketNumber}</p>
+  </div>
 </div>
   )
 }
