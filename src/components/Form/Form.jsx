f7959298ee20c9d8b7ffc693eeed546a48c48a76
Roman Klaška <aksalk@gmail.com>
2025-03-22 12:46:41 +0100

    takmer vsetko urobene

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,7 +14,6 @@ const Form = ({ onSetFormData }) => {
   const submitHandler = (event) => {
     event.preventDefault();
     onSetFormData(formData);
-    console.log("odoslane udaje:", formData);
   };
 
   // Aktualizacia textovych inputov
